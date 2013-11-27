2b53b927930eaf9162e4dd45f54323bf3b5ca538
Loqman <loqmanhakimi@gmail.com>
2013-11-27 20:36:59 +0330

    Add test file to test API interface

--- examples/test.php
+++ examples/test.php
@@ -5,3 +5,13 @@
  * Time: 8:31 PM
  * License: GPL
  */
+require "../lib/khamenei_api.php";
+
+$api = new KhameneiAPI();
+$api->type(KhameneiAPI::TYPE_ARTICLE)
+    ->limit(8)
+    ->offset(6)
+    ->order_by(KhameneiAPI::DATE_ASC)
+    ->from_year(1387)
+    ->to_year(1390)
+    ->get();
